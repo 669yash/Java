6d96d8b909c39301b6c9eaa5430d97f36fabea14
Yash Anil Mali <139006542+669yash@users.noreply.github.com>
2024-03-12 09:37:08 +0530

    Add files via upload

---
+++ bank.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+class bankaccount {
+    int account_no;
+    int account_balance;
+    String name;
+
+    public bankaccount(int account_no, int account_balance, String name) {
+
+    }
+
+    public bankaccount() {
+
+    }
+
+    public void getinfo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Name: ");
+        name = sc.nextLine();
+        System.out.println("Enter account no: ");
+        account_no = sc.nextInt();
+        System.out.println("Enter account balance: ");
+        account_balance = sc.nextInt();
+    }
+
+    public void dispinfo() {
+        System.out.println("Name:" + name);
+        System.out.println("Acoount no.:" + account_no);
+        System.out.println("Account balance:" + account_balance);
+    }
+    public void Deposite(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Amount to Deposite: ");
+        int Depo = sc.nextInt();
+        account_balance +=Depo;
+        System.out.println("Updated Balance: " + account_balance);
+
+    }
+
+    public void Withdraw(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Amount to Withdraw: ");
+        int with = sc.nextInt();
+        if(with>account_balance)
+            System.out.println("Cannot WithDraw..!");
+        else
+            account_balance -=with;
+        System.out.println("Updated Balance: " + account_balance);
+    }
+}
+class savingaccount extends bankaccount{
+    int limit;
+    public savingaccount(int account_no, int account_balance, String name){
+        super(account_no, account_balance, name);
+        limit=1000;
+    }
+
+}
+
+public class bank {
+    public static void main(String[] args) {
+        bankaccount b1 = new bankaccount();
+        b1.getinfo();
+        b1.dispinfo();
+        b1.Deposite();
+        b1.Withdraw();
+    }
+}
